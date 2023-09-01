478a812e6e896818c401c633e5c1b1c3dbbddb5b
Ildar <khalfin94@gmail.com>
2023-09-01 17:30:03 +0600

    Added folder structure, added index.php and CsvManager Class

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+use flip\CsvManager;
+
+require __DIR__ . '/../src/CsvManager.php';
+
+$host = '127.0.0.1';
+$dbName = 'flip';
+$username = 'root';
+$password = 'Warlord@94$';
+
+$pdo = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+
+$csv = new CsvManager(__DIR__ . '/../vendor/users.csv');
+$csv->csvToDb($pdo);
---
+++ src/CsvManager.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace flip;
+
+class CsvManager
+{
+    private string $csvFile;
+
+    public function __construct($csvFilePath)
+    {
+        $this->csvFile = $csvFilePath;
+    }
+
+    /**
+     * Create a PDO object for DB connection
+     *
+     * @param object $pdo A PDO object
+     * @return void
+     */
+    public function csvToDb(object $pdo): void
+    {
+        if (($file = fopen($this->csvFile, 'r')) !== false) {
+            $pdo->beginTransaction();
+
+            fgetcsv($file);
+
+            $sqlInsert = <<<SQL
+        INSERT INTO users (firstname, lastname, email, address) VALUES (?, ?, ?, ?)
+SQL;
+
+            $stmt = $pdo->prepare($sqlInsert);
+
+            while (($row = fgetcsv($file, null, ";")) !== false) {
+                $stmt->execute($row);
+            }
+
+            $pdo->commit();
+
+            fclose($file);
+
+            echo 'Данные успешно импортированы.';
+        } else {
+            echo 'Не удалось открыть файл CSV.';
+        }
+    }
+}
